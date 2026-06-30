ce3bb7dcd441e46036a71af87b3516b5c6df4c9b
admuser <admuser@example.com>
2026-06-30 20:46:51 +0200

    commit

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region                   = var.aws_region
   profile                  = var.aws_profile != "" ? var.aws_profile : null
   shared_credentials_files = var.aws_shared_credentials_file != "" ? [var.aws_shared_credentials_file] : null
-  assume_role {
-    role_arn     = var.aws_assume_role_arn
-    session_name = "groupe-7-iac-terraform"
-  }
   default_tags {
     tags = local.common_tags
   }
