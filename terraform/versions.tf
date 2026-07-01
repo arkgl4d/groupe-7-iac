1d89a67465c89226d2feec8c7d98d822c133521a
admuser <admuser@example.com>
2026-07-01 11:02:41 +0200

    commit

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,4 +1,10 @@
 terraform {
+  
+  backend "s3" {
+    bucket = "tf-state-group7-ynov"
+    key    = "group7/terraform.tfstate"
+    region = "eu-west-1"
+  }
   required_version = ">= 1.5.0"
 
   required_providers {
